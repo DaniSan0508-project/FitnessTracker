5763912c5c81bcf3ce830a49c00cd048d97290cd
DaniSan0508-project <danielsantoscarneiro0508@gmail.com>
2023-08-14 01:37:09 -0300

    Aplicativo iniciado , botão de Imc e tela de imc implementados.

---
+++ app/src/main/java/co/tiagoaguiar/fitnesstracker/MainActivity.kt
@@ -0,0 +1,22 @@
+package co.tiagoaguiar.fitnesstracker
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.LinearLayout
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var btnImc: LinearLayout
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnImc = findViewById(R.id.btn_imc)
+        btnImc.setOnClickListener {
+            val intent = Intent(this, ImcActivity::class.java)
+            startActivity(intent)
+        }
+
+
+    }
+}
